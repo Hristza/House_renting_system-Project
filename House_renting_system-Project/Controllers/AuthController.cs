7af766c85f5aac8ab857cbf8cd88667f808c1d51
Hristza1 <Win11@DESKTOP-KCIAQHM>
2026-03-31 08:17:44 +0300

    Add project files.

---
+++ House_renting_system-Project/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using House_renting_system_Project.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace House_renting_system_Project.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return View();
+        }
+    }
+}
