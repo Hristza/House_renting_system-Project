6246a5f6f0ca8020713fc35d9abb4b5f91ddef64
Hristofor Georgiev <161021759+Hristza@users.noreply.github.com>
2026-04-21 05:11:44 +0300

    Yea kinda adding to my repository right now

--- House_renting_system-Project/Controllers/HouseController.cs
+++ House_renting_system-Project/Controllers/HouseController.cs
@@ -1,100 +1,127 @@
 ﻿using House_renting_system_Project.Data.Data;
 using House_renting_system_Project.Data.Data.Entities;
-using House_renting_system_Project.Models;
 using House_renting_system_Project.Models.House;
+using House_renting_system_Project.Models.House.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Security.Claims;
 
 namespace House_renting_system_Project.Controllers
 {
     public class HouseController : Controller
     {
-        private readonly HouseRentingDbContext _db;
+        private readonly HouseRentingDbContext context;
 
-        public HouseController(HouseRentingDbContext databaseInstance)
+        public HouseController(HouseRentingDbContext context)
         {
-            _db = databaseInstance;
+            this.context = context;
         }
-
         [HttpGet]
         public async Task<IActionResult> AllHouses()
         {
-            var allProperties = await _db.Houses
-                .Select(h => new Models.HousesViewModel
-                {
-                    Id = h.Id,
-                    Name = h.Title,
-                    Address = h.Address,
-                    ImageUrl = h.ImageUrl
-                })
-                .ToListAsync();
-
-            return View(allProperties);
+            var currentUsersId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var housesViewModel = await context.Houses
+            .AsNoTracking()
+            .Select(h => new HousesViewModel
+            {
+                Id = h.Id,
+                Name = h.Title,
+                Address = h.Address,
+                ImageUrl = h.ImageUrl,
+                CurentUserIsOwner = h.AgentId == currentUsersId
+            })
+            .ToListAsync();
+            ViewBag.Title = "All houses";
+            return View(housesViewModel);
         }
-
         [HttpGet]
-        public async Task<IActionResult> Details(int id)
+        public async Task<IActionResult> Details(int Id) 
         {
-            var targetHouse = await _db.Houses.FirstOrDefaultAsync(h => h.Id == id);
+            var searched = await context.Houses
+                .Include(h => h.Agent)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(h => h.Id == Id);
 
-            if (targetHouse == null)
+            var model = new HouseDitailViewModel()
             {
-                return BadRequest(); // Връщаме грешка 400 Bad Request според условието
-            }
-
-            // Правим мапване към ViewModel, както се изисква в Упражнение 3
-            var houseDetailsModel = new Models.House.HouseDetailsViewModel
-            {
-                Id = targetHouse.Id,
-                Title = targetHouse.Title,
-                Address = targetHouse.Address,
-                Description = targetHouse.Description,
-                ImageUrl = targetHouse.ImageUrl,
-                PricePerMonth = targetHouse.PricePerMonth
-                // Ако имаш Category и Agent, ги добавяш тук
+                Id = searched.Id,
+                Address = searched.Address,
+                ImageUrl = searched.ImageUrl,
+                Description = searched.Description,
+                CreatedBy = searched.Agent.UserName,
+                Price =searched.PricePerMonth,
+                Name = searched.Title
             };
 
-            return View(houseDetailsModel);
+            return View(model);
         }
-
         [HttpGet]
-        public IActionResult CreateHouse()
+        [Authorize]
+        public async Task<IActionResult> CreateHouse()
         {
-            return View();
+            List<CategoryViewModel> ListOfCategories = await context.Categories
+            .AsNoTracking()
+            .Select(c => new CategoryViewModel
+            {
+                Id = c.Id,
+                Name = c.Name,
+            })
+            .ToListAsync();
+            var houseCategories = new HouseFormViewModel()
+            {
+                Categories = ListOfCategories
+            };
+            return View(houseCategories);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateHouse(HouseFormViewModel formModel)
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateHouse(HouseFormViewModel model)
         {
+
+            var houseCategories = await context.Categories
+                .AsNoTracking()
+                .Select(c => new CategoryViewModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                })
+                .ToListAsync();
+
             if (!ModelState.IsValid)
             {
-                return View(formModel);
+                
+                model.Categories = houseCategories;
+                return View(model);
+            }
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            bool addressExists = await context.Houses
+                .AnyAsync(h => h.Address.ToLower() == model.Address.ToLower());
+
+            if (addressExists)
+            {
+                model.Categories = houseCategories;
+                ModelState.AddModelError("Address", "This address is already registered");
+                return View(model);
             }
 
-            // Леко променена логика за проверка на адреса
-            bool isAddressTaken = await _db.Houses
-                .AnyAsync(prop => prop.Address.ToLower() == formModel.Address.ToLower());
-
-            if (isAddressTaken)
+            var newHouse = new House
             {
-                ModelState.AddModelError(nameof(formModel.Address), "This address is already registered in the system.");
-                return View(formModel);
-            }
-
-            var houseToInsert = new House
-            {
-                Title = formModel.Title,
-                Address = formModel.Address,
-                Description = formModel.Description,
-                ImageUrl = formModel.ImageUrl,
-                PricePerMonth = formModel.PricePerMonth
+                Title = model.Title,
+                Address = model.Address,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                PricePerMonth = model.PricePerMonth,
+                CategoryId = model.SelectedCategoryId,
+                AgentId = userId
             };
 
-            _db.Houses.Add(houseToInsert);
-            await _db.SaveChangesAsync();
+            context.Houses.Add(newHouse);
+            await context.SaveChangesAsync();
 
             return RedirectToAction(nameof(AllHouses));
         }
@@ -103,107 +130,21 @@ namespace House_renting_system_Project.Controllers
         [Authorize]
         public async Task<IActionResult> MyHouses()
         {
-            // В момента връщаме празен списък, докато не вържем логиката за конкретния потребител
-            var userProperties = new List<Models.HousesViewModel>();
-            return View(userProperties);
-        }
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        [HttpGet]
-        [Authorize]
-        public async Task<IActionResult> Edit(int id)
-        {
-            var houseToEdit = await _db.Houses.FindAsync(id);
-            if (houseToEdit == null)
-            {
-                return BadRequest();
-            }
-
-            var model = new HouseFormViewModel
-            {
-                Title = houseToEdit.Title,
-                Address = houseToEdit.Address,
-                Description = houseToEdit.Description,
-                ImageUrl = houseToEdit.ImageUrl,
-                PricePerMonth = houseToEdit.PricePerMonth
-            };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> Edit(int id, HouseFormViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            var houseToUpdate = await _db.Houses.FindAsync(id);
-            if (houseToUpdate == null)
-            {
-                return BadRequest();
-            }
-
-            houseToUpdate.Title = model.Title;
-            houseToUpdate.Address = model.Address;
-            houseToUpdate.Description = model.Description;
-            houseToUpdate.ImageUrl = model.ImageUrl;
-            houseToUpdate.PricePerMonth = model.PricePerMonth;
-
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(Details), new { id = houseToUpdate.Id });
-        }
-
-        [HttpGet]
-        [Authorize]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var houseToDelete = await _db.Houses.FindAsync(id);
-            if (houseToDelete == null)
-            {
-                return BadRequest();
-            }
-
-            var model = new Models.House.HouseDetailsViewModel
-            {
-                Id = houseToDelete.Id,
-                Title = houseToDelete.Title,
-                Address = houseToDelete.Address,
-                ImageUrl = houseToDelete.ImageUrl
-            };
-
-            return View(model);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [Authorize]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var house = await _db.Houses.FindAsync(id);
-            if (house != null)
-            {
-                _db.Houses.Remove(house);
-                await _db.SaveChangesAsync();
-            }
-
-            return RedirectToAction(nameof(AllHouses));
-        }
-
-        [HttpPost]
-        [Authorize]
-        public IActionResult Rent(int id)
-        {
-            // Логика за наемане
-            return RedirectToAction(nameof(MyHouses));
-        }
-
-        [HttpPost]
-        [Authorize]
-        public IActionResult Leave(int id)
-        {
-            // Логика за напускане на къщата
-            return RedirectToAction(nameof(MyHouses));
+            var houses = context.Houses
+                .Where(h => h.AgentId == userId)
+                .Select(h => new HousesViewModel
+                {
+                    Address = h.Address,
+                    ImageUrl = h.ImageUrl,
+                    Name = h.Title,
+                    Id = h.Id,
+                    CurentUserIsOwner = true
+                })
+                .ToListAsync();
+            ViewBag.Title = "My houses";
+            return View(nameof(AllHouses), houses);
         }
     }
 }
--- House_renting_system-Project/Program.cs
+++ House_renting_system-Project/Program.cs
@@ -15,26 +15,26 @@ namespace House_renting_system_Project
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
             builder.Services.AddDbContext<HouseRentingDbContext>(opt => opt.UseSqlServer(connectionString));
-
-
+            
+            
 
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>(
                 opt =>
-                {
-                    opt.User.RequireUniqueEmail = true;
-                    opt.Password.RequireNonAlphanumeric = false;
-                    opt.Password.RequiredLength = 6;
-                    opt.Password.RequireLowercase = false;
-                    opt.Password.RequireUppercase = false;
-                    opt.SignIn.RequireConfirmedEmail = false;
-                }
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 6;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.SignIn.RequireConfirmedEmail = false;
+            }
             )
                 .AddEntityFrameworkStores<HouseRentingDbContext>()
                 .AddDefaultTokenProviders();
 
             builder.Services.ConfigureApplicationCookie(options =>
             {
-                options.LogoutPath = "/User/Login";
+                options.LogoutPath = "/Auth/Login";
                 options.AccessDeniedPath = "/User/AccessDenied";
             });
 
